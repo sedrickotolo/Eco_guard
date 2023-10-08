bb83357105bb1de528e44a9c1503c305a2af5743
sedrickotolo <sedricksedu2@gmail.com>
2023-10-08 18:42:12 +0300

    new change

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,13 +1,3 @@
-import pandas as pd
-import geopandas as gpd
-
-# Load air pollution data from global.csv
-air_pollution_data = pd.read_csv('global.csv')
-
-# Load Uganda district boundaries GeoJSON file
-uganda_districts = gpd.read_file('uganda_districts.geojson')
-
-air_pollution_data.head(5)
 
 
 import pandas as pd
